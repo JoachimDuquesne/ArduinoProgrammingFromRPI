757d724a3509afab7579ebec9749f67bb28e808b
EscapeRush <vincent@escaperush.com>
2017-07-31 17:52:20 +0200

    initial upload

---
+++ upload.sh
@@ -0,0 +1,2 @@
+# upload le sketch firmware.hex sur un arduino uno connectée sur /dev/ttyACM0
+avrdude -C/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:firmware.hex:i 
