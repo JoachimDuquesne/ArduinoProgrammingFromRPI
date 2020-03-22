fec15875698677660d49bf7b358b77af5f28a0fe
JoachimDuquesne <13562415+JoachimDuquesne@users.noreply.github.com>
2020-03-22 17:19:39 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,2 +1,3 @@
-# upload le sketch firmware.hex sur un arduino uno connectée sur /dev/ttyACM0
-avrdude -C/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:firmware.hex:i 
+# upload le sketch firmware.hex sur un arduino uno connectÃ©e sur /dev/ttyACM0
+#avrdude -C/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:firmware.hex:i  ??? NOT WORKING. KEEPING FOR LEGACY
+avrdude -q -V -p atmega328p -C /etc/avrdude.conf -D -c arduino -b 57600 -P /dev/ttyUSB0 -U flash:w:firmware.hex:i
